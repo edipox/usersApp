c74753a8ca310af852e1ec91f52e3f5e82b3b6ce
Edipo da Silva <edipo.dasilva@trustyou.net>
2022-02-12 13:25:48 -0300

    [SETUP] Add roles relationship

--- grails-app/domain/usersapp/User.groovy
+++ grails-app/domain/usersapp/User.groovy
@@ -5,13 +5,12 @@ class User {
     String name
     String description
     String email
-    String type
+    Role role
     String encryptedPassword
 
     static constraints = {
         name maxSize: 255
         email maxSize: 255
-        type maxSize: 255
         encryptedPassword maxSize: 255
         description maxSize: 255, nullable: true
     }
